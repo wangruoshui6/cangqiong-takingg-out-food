b02fed3ef29d81f218a238819926fb237fbf5b14
wangruoshui <16633445526@163.com>
2026-03-05 16:49:41 +0800

    营业额数据统计功能代码开发

---
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -0,0 +1,18 @@
+package com.sky.service;
+
+import com.sky.vo.TurnoverReportVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+
+public interface ReportService {
+    /**
+     * 统计指定时间区域内的订单额
+     * @param begin
+     * @param end
+     * @return
+     */
+    TurnoverReportVO getTurnoverStatistic(LocalDate begin,LocalDate end);
+}
