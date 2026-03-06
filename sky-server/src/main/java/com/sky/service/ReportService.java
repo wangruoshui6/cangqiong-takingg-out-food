2706164d995b7d9b2943dd7d8d51558cbb05ba70
wangruoshui <16633445526@163.com>
2026-03-06 11:52:47 +0800

    订单排名统计功能代码开发

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -1,6 +1,7 @@
 package com.sky.service;
 
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
@@ -31,4 +32,11 @@ public interface ReportService {
      * @return
      */
     OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end);
+    /**
+     * 统计指定时间区域内的排名数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
 }
